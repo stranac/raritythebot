e1568df28fab8b205de333c8dcbfe55b67529030
stranac <stranac@hotmail.com>
2013-11-10 16:19:13 +0100

    Added paste command. Changed the help system a bit.

--- commands.py
+++ commands.py
@@ -1,5 +1,14 @@
+import requests
+import inspect
+
+from utils import redent
+
+import config
+
+
 irc_commands = {}
 admin_commands = {}
+command_help = {}
 
 
 class irc_command(object):
@@ -9,22 +18,56 @@ class irc_command(object):
     """
     def __init__(self, level):
         self.commands = admin_commands if level == 'admin' else irc_commands
+        self.level = level
 
     def __call__(self, f):
         self.commands[f.__name__] = f
+        if self.level != 'admin':
+            command_help[f.__name__] = inspect.getdoc(f)
         return f
 
 
 @irc_command('user')
 def help(args):
-    return '%source: get link to my source code'
+    """
+    help: show all available commands.
+    help <command>: show help for a single command.
+    """
+    args = args.strip()
+    if not args:
+        commands = ', '.join(command_help)
+        return ('Available commands: {}. Use {}help <command> to get help '
+                'on a specific command.').format(commands, config.trigger)
+    return command_help.get(args, 'No help for command: args')
+
 
 @irc_command('user')
 def source(args):
+    """
+    Get a link to my source code.
+    """
     return 'https://github.com/stranac/raritythebot'
+
+@irc_command('user')
+def paste(code):
+    """
+    Paste a piece of code to http://bpaste.net/
+    Use a semicolon to end a line, multiple semicolons to end blocks.
+    """
+    data = {'code': redent(code),
+            'language': 'python',
+            'webpage': '',
+            'private': 'on'
+            }
+    r = requests.post('http://bpaste.net/', data)
+    return r.url.encode()
 
 
 @irc_command('admin')
 def say_to(args):
+    """
+    args: <user or channel> <message>
+    Send message to the target.
+    """
     target, _, msg = args.partition(' ')
     return '%s >> %s' % (msg, target)
